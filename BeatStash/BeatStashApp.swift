455a5217af20a7018163ae2ef2e3fc836bed4537
Stitch <stitchlabs@proton.me>
2026-09-09 06:13:29 +0100

    Scaffold BeatStash macOS app in Xcode

---
+++ BeatStash/BeatStashApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatStashApp.swift
+//  BeatStash
+//
+//  Created by Studio on 09/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct BeatStashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeatStash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BeatStash
+//
+//  Created by Studio on 09/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
